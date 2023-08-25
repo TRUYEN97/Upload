bc92aefd2176ac16c0d2ad83df118a1415333ef9
SQT <samtruyen60@mail.com>
2023-08-25 09:33:38 +0700

    update

---
+++ store/backup/Camera-CCD/lib_install.bat
@@ -0,0 +1,13 @@
+python.exe -m pip install --upgrade pip
+pip install torch
+pip install torchvision
+pip install opencv-python
+pip install matplotlib
+pip install pandas
+pip install tqdm
+pip install PyYAML
+pip install seaborn
+pip install scipy
+pip install pyzbar
+pip install pylibdmtx
+vcredist_x64.exe
